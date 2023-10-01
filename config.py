fa1190e2e3ed03313e635c225a7818ee5174aa73
skap3214 <kapadiasoami@gmail.com>
2023-10-01 12:04:30 -0400

    cleaned up a bit, and kind works

---
+++ config.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    MODEL_PATH = './models/v1/AGPT_large.pth'
+    BATCH_SIZE = 64
+    BLOCK_SIZE = 256
+    EVAL_INTERVAL = 500
+    EPOCHS = 5000
+    LR = 3e-3
+    DEVICE = "cpu"
+    EVAL_ITERS = 200
+    MANUAL_SEED = 22
+    DATA = './datasets/shakespeare.txt'
+    N_EMBD = 384
+    NUM_BLOCKS = 6
+    DROPOUT = 0.2
+    HEADS = 6 #Should be divisible by N_EMBD
+
+class SmallConfig:
+    MODEL_PATH = './models/v1/AGPT_small.pth'
+    BATCH_SIZE = 32
+    BLOCK_SIZE = 8 #Context Lenth of the model
+    EVAL_INTERVAL = 500
+    EPOCHS = 5000
+    LR = 1e-3
+    DEVICE = "cpu"
+    EVAL_ITERS = 200
+    MANUAL_SEED = 22
+    DATA = './datasets/shakespeare.txt'
+    N_EMBD = 32
+    NUM_BLOCKS = 3 #Number of Multi Attention Blocks
+    DROPOUT = 0.1 #percentage of weights which will be randomly zeroed
+    HEADS = 4 #Should be divisible by N_EMBD
--- generate.py
+++ generate.py
@@ -1,16 +1,14 @@
 import torch
-from model import NotGPT, adjust_tensor_to_block_size, decode, encode, BLOCK_SIZE, DEVICE
-
-MODEL_PATH = 'bigram_language_model.pth'
+from model import AGPT, adjust_tensor_to_block_size, decode, encode, Config
 
 # Load model
-model = NotGPT().to(DEVICE)
-model.load_state_dict(torch.load(MODEL_PATH))
+model = AGPT().to(Config.DEVICE)
+model.load_state_dict(torch.load(Config.MODEL_PATH))
 model.eval()
 
 def generate_response(query):
-    ten_query = torch.tensor([encode(query)], dtype=torch.long, device=DEVICE)
-    enc_query = adjust_tensor_to_block_size(ten_query, BLOCK_SIZE)
+    ten_query = torch.tensor([encode(query)], dtype=torch.long, device=Config.DEVICE)
+    enc_query = adjust_tensor_to_block_size(ten_query, Config.BLOCK_SIZE)
     generated_tokens = model.generate(enc_query, 500)[0]
     return decode(generated_tokens.tolist())
 
